8ac63fdbbbdbcd744006cd27ecb85eb30f7e6630
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-11-28 00:22:48 +0000

    comit 1

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("enter your word: ");
+        String word = System.console().readLine();
+
+        //loop through the word one way and save
+        //loop through the word the opposit wsy and save
+        //compare words.
+
+        while(int i = word.length(); i < word.length(); i++   ){
+
+        }
+    }
+}
