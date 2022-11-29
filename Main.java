abbd804fe88855c44ad1afd01a98cfd8cc6f02d1
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-11-29 21:11:14 +0000

    changes

--- Main.java
+++ Main.java
@@ -2,13 +2,22 @@ class Main {
     public static void main(String[] args) {
         System.out.println("enter your word: ");
         String word = System.console().readLine();
+        int length = word.length();
+        int forward = 0;
+        int backward = length - 1;
 
-        //loop through the word one way and save
-        //loop through the word the opposit wsy and save
-        //compare words.
+        // loop through the word one way and save
+        // loop through the word the opposit wsy and save
+        // compare words
 
-        while(int i = word.length(); i < word.length(); i++   ){
+        while (backward > forward) {
+            char forwardChar = word.charAt(forward++);
+            char backwardChar = word.charAt(backward--);
+            if (forwardChar != backwardChar) {
+                // do something
+            }
 
         }
+
     }
 }
