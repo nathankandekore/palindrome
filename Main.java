3deef9c326dcf712fd58b715f80e6a55b1fc6de5
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-11-30 23:36:33 +0000

    instructions

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of units that may be read the same way in either
+direction. Examples of strict palindromes include “ABBA”, “madam”, “radar”, “kayak”, and “step on no pets”.
+Write a program that reads a text and detects whether the text is a strict palindrome
+*/
+
 class Main {
     public static void main(String[] args) {
         System.out.println("enter your word: ");
