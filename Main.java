99978914b65ff490a5f0e4eeee788412d40ba363
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-11-30 23:29:21 +0000

    forever loop

--- Main.java
+++ Main.java
@@ -2,21 +2,29 @@ class Main {
     public static void main(String[] args) {
         System.out.println("enter your word: ");
         String word = System.console().readLine();
-        int length = word.length();
-        int forward = 0;
-        int backward = length - 1;
 
-        // loop through the word one way and save
-        // loop through the word the opposit wsy and save
-        // compare words
+        int length = word.length() - 1;
+        int counter = 0;
+        boolean endProgram = false;
 
-        while (backward > forward) {
-            char forwardChar = word.charAt(forward++);
-            char backwardChar = word.charAt(backward--);
-            if (forwardChar != backwardChar) {
-                // do something
+        while (endProgram == false) {
+            while (counter < length / 2) {
+                if (word.charAt(counter) == word.charAt(length)) {
+
+                    counter++;
+                    length--;
+                    System.out.println("letter matched");
+                }
+
+                else {
+                    endProgram = true;
+                    System.out.println("not a palindrome");
+                }
             }
+        }
 
+        if (counter > length && endProgram == false) {
+            System.out.println("PALINDROME!!!!!!");
         }
 
     }
